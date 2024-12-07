fee47bc1f075d16ff2b48cd80206bf68ebfa8c3f
cesarlunava <a01254603@tec.mx>
2024-12-06 21:39:50 -0700

    datosFormulario
    circulación

--- backend/Entrada_Datos.js
+++ backend/Entrada_Datos.js
@@ -1,5 +1,5 @@
 //Llamo al archivo conexio.js
-require('./conexion');
+/*require('./conexion');
 //Para despues llamar una funcion del mismo
 conectar();
 //const serie = req.body.serie;
@@ -7,5 +7,45 @@ const Nombre = req.body.Nombre;
 const ClavedeModelo = req.body.ClavedeModelo;
 const email = req.body.email;
 const password = req.body.password;
+conectar.end();*/
 
-conectar.end();
+
+const express = require('express');
+const mysql = require('mysql');
+
+const app = express();
+app.use(express.json()); // Permite procesar datos en formato JSON
+
+// Configura la conexión a la base de datos de Railway
+const connection = mysql.createConnection({
+  host: 'autorack.proxy.rlwy.net',
+  user: 'root',
+  password: 'hqZnvoLyUozaAIyeIfqznWRGZdelYodv',
+  database: 'railway'
+});
+
+// Ruta para recibir los datos del formulario
+app.post('/save-data', (req, res) => {
+  const { id, Nombre, ClavedeModelo, password, email} = req.body;
+
+  // Inserta los datos en la base de datos
+  connection.query(
+    'INSERT INTO contact_form (id, email, ClavedeModelo, password, Nombre) VALUES (?, , )',
+    [id, email, ClavedeModelo, password, Nombre],
+    (error, results, fields) => {
+      if (error) {
+        console.error('Error al guardar los datos:', error);
+        res.status(500).send('Error al guardar los datos');
+      } else {
+        res.send('Datos guardados exitosamente');
+      }
+    }
+  );
+});
+
+app.listen(3000, () => {
+  console.log('Servidor iniciado en el puerto 3000');
+});
+
+
+
--- backend/conexion.js
+++ backend/conexion.js
@@ -1,10 +1,9 @@
-function conectar () {
+/*function conectar () {
 const urlDataBase = "mysql://root:hqZnvoLyUozaAIyeIfqznWRGZdelYodv@mysql.railway.internal:3306/railway"
 
 const connection = mysql.createConnection(urlDataBase);
 
 module.exports = connection;
-}
+}*/
 
-
-
+// app.js
